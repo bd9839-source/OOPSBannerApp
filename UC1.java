68ff6aa3a413f5f97e44c56681c6452bc3e0e092
bd9839-source <bd9839@srmist.edu.in>
2026-03-03 15:38:04 +0530

    first commit

---
+++ UC1.java
@@ -0,0 +1,5 @@
+public class OOPsBannerApp{
+	public static void main(Sring[] args){
+		System.out.println("OOPs");
+	}
+}
---
+++ UC2.java
@@ -0,0 +1,11 @@
+public class UC2s{
+
+    public static void main(String[] args) {
+        System.out.println("****** ****** *    * ******");
+        System.out.println("*    * *    * *    * *    *");
+        System.out.println("*    * *    * *    * *    *");
+        System.out.println("*    * *    * *    * *    *");
+        System.out.println("*    * *    * *    * *    *");
+        System.out.println("****** ****** ****** ******");
+    }
+}
